8155d1818c0b9dffcfc167a8f5de11e05c0b55ce
gdutewei <1172177810@qq.com>
2018-11-03 23:54:54 +0800

    MOD:作品集根据clientWidth进行裁剪显示

--- src/components/rWorks/cmpt.js
+++ src/components/rWorks/cmpt.js
@@ -34,12 +34,23 @@ ResumeWorksCtrl.$inject = ["dataExtend","resumeData","$rootScope","$scope"];
 
 function ResumeWorksCtrl(dataExtend,resumeData,$rootScope, $scope) {
     let vm = this;
-
+	let clientWidth = document.body.clientWidth;
+	if (clientWidth > 600 && clientWidth < 880) {
+		resumeData.cn.works.worksList = resumeData.cn.works.worksList.slice(0,1)
+	} else if (clientWidth > 880) {
+		resumeData.cn.works.worksList = resumeData.cn.works.worksList.slice(0,2)
+	}
     dataExtend.extend(vm, resumeData.cn.works);
 
     $rootScope.$on("switchLang",function(evt,data){
 
-        let extendData = Number(data) == 2?resumeData.en.works:resumeData.cn.works;
+		let extendData = Number(data) == 2?resumeData.en.works:resumeData.cn.works;
+		
+		if (clientWidth > 600 && clientWidth < 880) {
+			extendData.worksList = extendData.worksList.slice(0,1)
+		} else if (clientWidth > 880) {
+			extendData.worksList = extendData.worksList.slice(0,2)
+		}
 
         vm.isEng = Number(data) == 2;
 
